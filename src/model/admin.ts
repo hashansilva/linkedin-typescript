a933a4d4bfd92ca35275a323d13595b59f686763
hashan.silva <hashan.silva@gmail.com>
2023-04-29 22:56:47 +0530

    challenge - encapsulation

--- src/model/admin.ts
+++ src/model/admin.ts
@@ -1,10 +1,18 @@
 import { User } from "./user"
 
 export class Admin extends User{
+
+    public readonly yearBorn: number;
+
     constructor(firstName:string, lastName:string, email:string){
         super();
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.yearBorn = this.getYear();
+    }
+
+    private getYear():number{
+        return this.dob.getFullYear();
     }
 }
--- src/model/user.ts
+++ src/model/user.ts
@@ -1,7 +1,9 @@
 export class User{
+    public readonly id: string;
     public firstName:string;
     public lastName:string;
     public email:string;
+    protected dob:Date;
 
     public get fullName():string{
         return `${this.firstName} ${this.lastName}`;
